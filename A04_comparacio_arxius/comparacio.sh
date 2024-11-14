475d78c0285e9de849557abd91b491b52d3cb8fc
AraceliGG17 <146652579+AraceliGG17@users.noreply.github.com>
2024-11-14 19:44:14 +0100

    Update comparacio.sh

--- A04_comparacio_arxius/comparacio.sh
+++ A04_comparacio_arxius/comparacio.sh
@@ -14,7 +14,7 @@ mida_archivo2=$(ls -la $2 | cut -d " " -f5 )
 if [ $mida_archivo1 -eq $mida_archivo2 ]; then
 	echo "El archivo1 y el archivo2 tienen la misma medida"
 elif [ $mida_archivo1 -gt $mida_archivo2 ]; then
-	echo "el $1 es mayor"
+	echo "el $1 es mayor: $mida_archivo1 bytes"
 else
-	echo "el $2 es mayor"
+	echo "el $2 es mayor: $mida_archivo2 bytes"
 fi
